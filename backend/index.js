79f3da8a262a21644dae511d9decfe1dac6ac5c3
temesgen-abebayehu <temesgensoftware21@gmail.com>
2024-12-13 20:30:10 +0300

    add cookie-parser middleware for handling cookies in the backend

--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,14 @@ import dotenv from 'dotenv';
 
 import AuthRouter from './routes/auth.route.js';
 import connectDB from './config/db.js';
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 
 app.use("/api/auth", AuthRouter);
